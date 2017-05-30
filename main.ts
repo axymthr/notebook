cad529fc785e008df7c494553389b194a0d37253
awkshwayrd <mathur.aksh@gmail.com>
2017-05-30 23:22:13 +0530

    Final commit - more error handling and separated out  loader

--- main.ts
+++ main.ts
@@ -1,49 +1,12 @@
 import {Observable} from "rxjs";
+import {load, loadWithFetch} from "./loader";
 
 let output = document.getElementById("output");
 let button = document.getElementById("button");
 
 let click = Observable.fromEvent(button, "click");
 
-function load(url: string) {
-    return Observable.create(observer => {
-        let xhr = new XMLHttpRequest();
 
-        xhr.addEventListener("load", () => {
-            if(xhr.status === 200) {
-                let data = JSON.parse(xhr.responseText);
-                observer.next(data);
-                observer.complete();
-            } else {
-                observer.error(xhr.statusText);
-            }
-        });
-
-        xhr.open("GET", url);
-        xhr.send();
-    }).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
-}
-
-function loadWithFetch(url: string) {
-    return Observable.defer(() => 
-    {
-        return Observable.fromPromise(
-            fetch(url).then(r => r.json())
-        ));
-    });    
-}
-
-function retryStrategy({attempts = 4, delay = 1000}) {
-    return function(errors) {
-        return errors
-                .scan((acc, value) => {
-                    console.log(acc, value);
-                    return acc + 1;
-                }, 0)
-                .takeWhile(acc => acc < attempts)
-                .delay(delay);
-    }
-}
 
 function renderMovies(movies) {
     movies.forEach(m => {
@@ -53,7 +16,15 @@ function renderMovies(movies) {
     });
 }
 
-loadWithFetch("movies.json").subscribe(renderMovies);
+let subscription = 
+  load("movies.json")
+    .subscribe(renderMovies, 
+              e => console.log(`error: ${e}`),
+              () => console.log("complete!"));
+
+console.log(subscription);
+//subscription.unsubscribe();
+
 
 click.flatMap(e => loadWithFetch("movies.json"))
      .subscribe(
@@ -61,7 +32,3 @@ click.flatMap(e => loadWithFetch("movies.json"))
         e => console.log(`error: ${e}`),
         () => console.log("complete")
     );
-
-
-
-
