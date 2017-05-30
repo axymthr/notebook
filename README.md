93606d893a56aadf250b7b27aafbaec26e9ea691
awkshwayrd <mathur.aksh@gmail.com>
2017-05-30 22:40:11 +0530

    Load movies with whatwg and promises

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ node_modules/.bin/tyings install dt~es6-shim --global --save
 
 npm start
 
+Load movies with promise and whatwg
+node_modules/.bin/typings uninstall es6-shim --save --global
+node_modules/.bin/typings install dt~whatwg-streams --save --global
+node_modules/.bin/typings install dt~whatwg-fetch --save --global
--- main.ts
+++ main.ts
@@ -1,35 +1,66 @@
 import {Observable} from "rxjs";
 
-// import {Observable} from "rxjs/Observable";
-// import "rxjs/add/operator/map";
-// import "rxjs/add/operator/filter";
+let output = document.getElementById("output");
+let button = document.getElementById("button");
 
-let numbers = [1, 5, 10];
-let source = Observable.create(observer => {
+let click = Observable.fromEvent(button, "click");
 
-    let index = 0;
-    let produceValue = () => {
-        observer.next(numbers[index++]);
+function load(url: string) {
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
 
-        if(index < numbers.length) {
-            setTimeout(produceValue, 250);
-        }
-        else {
-            observer.complete();
-        }
+        xhr.addEventListener("load", () => {
+            if(xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
+        });
+
+        xhr.open("GET", url);
+        xhr.send();
+    }).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
+}
+
+function loadWithFetch(url: string) {
+    return Observable.defer(() => 
+    {
+        return Observable.fromPromise(
+            fetch(url).then(r => r.json())
+        ));
+    });    
+}
+
+function retryStrategy({attempts = 4, delay = 1000}) {
+    return function(errors) {
+        return errors
+                .scan((acc, value) => {
+                    console.log(acc, value);
+                    return acc + 1;
+                }, 0)
+                .takeWhile(acc => acc < attempts)
+                .delay(delay);
     }
+}
 
-    produceValue();
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
 
-}).map(n => n * 2)
-  .filter(n => n > 4);
-  
+loadWithFetch("movies.json").subscribe(renderMovies);
 
-source.subscribe(
-    value => console.log(`value: ${value}`),
-    e => console.log(`error: ${e}`),
-    () => console.log("complete")   
-);
+click.flatMap(e => loadWithFetch("movies.json"))
+     .subscribe(
+        renderMovies,
+        e => console.log(`error: ${e}`),
+        () => console.log("complete")
+    );
 
 
 
