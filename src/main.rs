e25d7a2ff470e5d5e767dac36ad5df7b48a3e783
awkshwayrd <mathur.aksh@gmail.com>
2022-01-09 12:37:06 +0530

    structs, iter, match, closure

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,33 @@ fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
     println!("Hello, {:?}", name);
-    let visitor_list = ["bert", "steve", "fred"];
-    let mut allow_them_in = false;
-    for visitor in &visitor_list {
-        if visitor == &name {
-            allow_them_in = true;
-        }
-    }
-    if allow_them_in {
-        println!("Welcome to the Treehouse, {}", name);
-    } else {
-        println!("Sorry, you aren't on the list.");
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
+
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You are not on the visitor list. Please leave."),
     }
 }
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
 
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
