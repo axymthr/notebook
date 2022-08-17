a79da35288c969f62ec07618a042dbb45e378930
axymthr <mathur.aksh@gmail.com>
2022-08-17 18:15:33 +0530

    chapter 7

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,16 +2,26 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import del from 'del';
-import { exec } from 'child_process';
 import eslint from 'gulp-eslint';
+import webpack from 'webpack-stream';
+import webpackConfig from './webpack.config.babel';
 
 const paths = {
-  allSrcJs: 'src/**/*.js',
+  allSrcJs: 'src/**/*.js?(x)',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**/*.js?(x)',
+  clientEntryPoint: 'src/client/app.js',
   gulpFile: 'gulpfile.babel.js',
+  webpackFile: 'webpack.config.babel.js',
+  clientBundle: 'dist/client-bundle.js?(.map)',
   libDir: 'lib',
+  distDir: 'dist',
 };
 
-gulp.task('clean', () => del(paths.libDir));
+gulp.task('clean', () => del([
+  paths.libDir,
+  paths.clientBundle,
+]));
 
 gulp.task('build', ['lint', 'clean'], () =>
   gulp.src(paths.allSrcJs)
@@ -19,12 +29,11 @@ gulp.task('build', ['lint', 'clean'], () =>
   .pipe(gulp.dest(paths.libDir))
 );
 
-gulp.task('main', ['build'], (callback) => {
-  exec(`node ${paths.libDir}`, (error, stdout) => {
-    console.log(stdout);
-    return callback(error);
-  });
-});
+gulp.task('main', ['lint', 'clean'], () =>
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir))
+);
 
 gulp.task('watch', () => {
   gulp.watch(paths.allSrcJs, ['main']);
@@ -36,8 +45,9 @@ gulp.task('lint', () =>
   gulp.src([
     paths.allSrcJs,
     paths.gulpFile,
+    paths.webpackFile,
   ])
   .pipe(eslint())
   .pipe(eslint.format())
-  .pipe(eslint.failAfterError())
+  // .pipe(eslint.failAfterError())
 );
